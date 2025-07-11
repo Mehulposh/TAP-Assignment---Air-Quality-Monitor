b8c3b8dfde428b65fa779472cd354b32246f35d4
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-11 19:00:47 +0530

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='text-center mt-10'>
+        <h1 className='text-4xl md:text-5xl font-bold mb-4 drop-shadow-lg' >🌍 Smart City Air Quality Monitor</h1>
+        <p className='text-xl text-blue-400'>Real-time environmental monitoring for your health and safety</p>
+    </div>
+  )
+}
+
+export default Header
