fe164d28cd9f0a631a8c91f2855aea22959a88de
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-12 21:08:27 +0530

    implement useIntersectionObserver hook to track visibility of elements with Intersection Observer API

--- src/customHooks/useIntersectionObserver.jsx
+++ src/customHooks/useIntersectionObserver.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const useIntersectionObserver = () => {
-  return (
-    <div>useIntersectionObserver</div>
-  )
+  const [visibleEle, setVisibleEle] = useState(new Set());
+  const observerRef = useRef(null);
+
+  useEffect(() => {
+    const elements = document.querySelectorAll('.fade-in');
+
+    observerRef.current = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting){
+          setVisibleEle(prev => new Set([...prev, entry.target.id]));
+        }
+      });
+    },{threshold: 0.1});
+
+    elements.forEach(ele => {
+      if(ele.id){
+        observerRef.current.observer(ele);
+      }
+    });
+
+    return () => {
+      if(observerRef.current){
+        observerRef.current.disconnect();
+      }
+    };
+  },[]);
+
+
+  return visibleEle;
 }
 
 export default useIntersectionObserver
