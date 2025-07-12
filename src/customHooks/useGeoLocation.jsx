98e2062afe85d5dbc29a780a01f9067f3d2536c9
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-12 20:46:17 +0530

    implement useGeoLocation hook to retrieve and manage user's geolocation with error handling

--- src/customHooks/useGeoLocation.jsx
+++ src/customHooks/useGeoLocation.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
 
 const useGeoLocation = () => {
-  return (
-    <div>useGeoLocation</div>
-  )
+  const [location , setLocation] = useState({
+    lat: null,
+    long: null,
+    address: 'Detecting...'
+  });
+
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if('geolocation' in navigator){
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const {latitude, longitude} = position.coords;
+          setLocation({
+            lat: latitude,
+            long: longitude,
+            address: `${latitude.toFixed(4)} , ${longitude.toFixed(4)}`
+          });
+
+          setError(null);
+        },
+
+        (error) => {
+          console.log("Location Error :", error);
+          setError('Unable to detect location . using default location');
+          setLocation({
+            lat: 19.0760,
+            long: 72.8777,
+            address: 'Mumbai, Maharashtra, India (default)'
+          });
+        },
+        {enableHighAccuracy: true, timeout: 10000, maximumAge: 300000}
+      );
+    }else{
+      setError('Geolocation not supported . using default location');
+      setLocation({
+            lat: 19.0760,
+            long: 72.8777,
+            address: 'Mumbai, Maharashtra, India (default)'
+          });
+    }
+  },[]);
+
+  return {location, error};
 }
 
 export default useGeoLocation
