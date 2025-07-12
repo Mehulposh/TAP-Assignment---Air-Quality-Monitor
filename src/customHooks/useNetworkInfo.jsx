0cb335090b153d85f75754c2756f1fcf8947b5bc
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-12 20:36:01 +0530

    added useNetworkInfo hook to manage and provide network status and connection details

--- src/customHooks/useNetworkInfo.jsx
+++ src/customHooks/useNetworkInfo.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const useNetworkInfo = () => {
-  return (
-    <div>useNetworkInfo</div>
-  )
-}
+  const [networkInfo, setNetworInfo] = useState({
+    online: navigator.onLine,
+    type: 'unknown',
+    speed: null
+  });
+
+  useEffect(() => {
+    const updateNetworlInfo = () => {
+      const connection  = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+      setNetworInfo({
+        online: navigator.onLine,
+        type: connection?.effectiveType || 'unknown',
+        speed: connection?.downlink || null
+      });
+    };
+
+    updateNetworlInfo();
+    window.addEventListener('online' , updateNetworlInfo);
+    window.addEventListener('offline' , updateNetworlInfo);
+
+    if('connection' in navigator){
+      navigator.connection.addEventListener('change' , updateNetworlInfo);
+    }
+
+    return () => {
+      window.removeEventListener('online' , updateNetworlInfo);
+      window.removeEventListener('offline' , updateNetworlInfo);
+      if('connection' in navigator){
+        navigator.connection.removeEventListener('change' , updateNetworlInfo);
+      }
+    };
+  },[]);
+
+  return networkInfo;
+};
 
 export default useNetworkInfo
