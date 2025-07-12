3eb807b7a0c8c6c9f62e4434b629fea6d6958b84
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-12 16:42:27 +0530

    added loader code

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Loader = () => {
-  return (
-    <div>Loader</div>
+const Loader = ({isLoading}) => {
+  return isLoading && (
+    <div className='text-center py-8 '>
+      <div className='animate-spin rounded-full h-12 w-12 border-b-2 border-white mx-auto mb-4'>
+        <p className='text-blue-100'>Loading Required Data...</p>
+      </div>
+    </div>
   )
 }
 
