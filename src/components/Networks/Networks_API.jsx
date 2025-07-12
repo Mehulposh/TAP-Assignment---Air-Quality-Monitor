9acd3515922b88b94f674a24219d4688c14755d5
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-12 19:05:20 +0530

    added Networks_API component to display network status and connection details

--- src/components/Networks/Networks_API.jsx
+++ src/components/Networks/Networks_API.jsx
@@ -1,8 +1,34 @@
+import { Wifi } from 'lucide-react';
 import React from 'react'
 
-const Networks_API = () => {
+const Networks_API = ({networkInfo}) => {
   return (
-    <div>Networks(API)</div>
+    <div>
+      <div>
+        {networkInfo.online ? (
+          <>
+            <Wifi className='w-5 h-5 text-green-500'/>
+            <span className='text-green-600'>
+              Online
+            </span>
+          </>
+        ) : (
+          <>
+            <Wifi className='w-5 h-5 text-red-500'/>
+            <span className='text-red-600'>
+              Offline
+            </span>
+          </>
+        )} 
+        <div className='e-3 h-3 bg-green-500 rounded-full animate-pulse'></div>
+      </div>
+      <div className='text-right'>
+        <div>
+          Connection: {networkInfo.type}
+        </div>
+        {networkInfo.speed && <div> Speed: {networkInfo.speed} Mbps</div>}
+      </div>
+    </div>
   )
 }
 
