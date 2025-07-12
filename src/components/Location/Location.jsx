6036c9c93afa443acf7b27ac3a733eff526efe95
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-12 18:58:54 +0530

    added Location component to display address with map pin icon

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,8 +1,16 @@
+import { MapPin } from 'lucide-react'
 import React from 'react'
 
-const Location = () => {
+const Location = ({location}) => {
   return (
-    <div>Location</div>
+    <div className='bg-white/90 backdrop-blur-sm rounded-lg p-4 mb-6 text-center text-gray-800'>
+      <div className='flex items-center justify-center gap-2'>
+        <MapPin className='w-5 h-5 text-blue-500'/>
+        <span>
+          {location.address}
+        </span>
+      </div>
+    </div>
   )
 }
 
