3c729f46c364f02807f09b5e0c55d63c72d2fb60
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-13 12:35:54 +0530

    fix: update document title to reflect application name; adjust Loader component structure

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,8 +4,9 @@ const Loader = ({isLoading}) => {
   return isLoading && (
     <div className='text-center py-8 mt-4 '>
       <div className='animate-spin rounded-full h-12 w-12 border-b-2 border-white mx-auto mb-4'>
-        <p className='text-blue-100'>Loading Required Data...</p>
+        
       </div>
+      <p className='text-blue-100'>Loading Required Data...</p>
     </div>
   )
 }
